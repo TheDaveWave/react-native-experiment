6b60b2134ba52b8bf578907681be66ba39f1e0f1
David Lindberg <david.wesley.lindberg@gmail.com>
2022-12-20 09:12:36 -0600

    created my first component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Clicker from './components/Clicker';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Clicker />
       <StatusBar style="auto" />
     </View>
   );
